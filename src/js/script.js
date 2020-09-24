dbd745ee392fbf4f9bb9b53f95e049ba529cd545
Ilya <36233549+cyberpunk3351@users.noreply.github.com>
2020-09-24 21:32:03 +0700

    edit navigation

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,18 @@
-@@include('jquery.min.js');
+@@include('../../node_modules/jquery/dist/jquery.js');
+@@include('../../node_modules/popper.js/dist/umd/popper.js');
 @@include('webp.js');
 @@include('alert.js');
+
+// Импортируем необходимые js-файлы Bootstrap 4
+@@include('../../node_modules/bootstrap/js/dist/util.js');
+@@include('../../node_modules/bootstrap/js/dist/alert.js');
+@@include('../../node_modules/bootstrap/js/dist/button.js');
+@@include('../../node_modules/bootstrap/js/dist/carousel.js');
+@@include('../../node_modules/bootstrap/js/dist/collapse.js');
+@@include('../../node_modules/bootstrap/js/dist/dropdown.js');
+@@include('../../node_modules/bootstrap/js/dist/modal.js');
+@@include('../../node_modules/bootstrap/js/dist/tooltip.js');
+@@include('../../node_modules/bootstrap/js/dist/popover.js');
+@@include('../../node_modules/bootstrap/js/dist/scrollspy.js');
+@@include('../../node_modules/bootstrap/js/dist/tab.js');
+@@include('../../node_modules/bootstrap/js/dist/toast.js');
